83cabdee42a979f34af2efab2ba5f98fafaf3b92
vimwitch <jchancehud@gmail.com>
2025-06-01 23:45:27 -0500

    build directory listing script

--- scripts/build_www_list.mjs
+++ scripts/build_www_list.mjs
@@ -6,11 +6,20 @@ const index_src = (await fs.readFile('./www/index.html')).toString()
 
 // look for insertion comments
 const files = await fs.readdir('./www')
-const marker_regex = /<!--build_www_list-->.*<!--build_www_list-->/gm
+
 const marker = '<!--build_www_list-->'
-const new_index_src = index_src.replace(marker_regex, `${marker}
+const marker_regex = /<!--build_www_list-->[\s\S]*?<!--build_www_list-->/g
+const match = index_src.match(marker_regex)
+if (!match) {
+  console.log('did not find script delimeter in html')
+}
+
+const out_html = 
+`${marker}
 ${files.map(name => `<a href="/${name}" target="_blank">${name.split('.')[0]}</a>`
 ).join('\n')}
-${marker}`);
+${marker}`
 
-console.log(new_index_src)
+const new_index_src = index_src.replace(match, out_html)
+
+await fs.writeFile('./www/index.html', new_index_src)
